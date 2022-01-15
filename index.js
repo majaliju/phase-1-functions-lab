4448fc624626aea32225d9b53d432a12af84891e
majaliju <majalijunyc@gmail.com>
2022-01-15 15:32:57 -0500

    commit tree 01ef4c560389798202441bad936c7382faf15918

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation){
+    const HQ = 42;
+    let distance = Math.abs(pickupLocation - HQ);
+    return distance;
+}
+
+function distanceFromHqInFeet (pickupLocation){
+    let distanceInFeet = distanceFromHqInBlocks(pickupLocation) * 264;
+    return distanceInFeet;
+}
+
+function distanceTravelledInFeet(start, destination){
+    let distanceTravelled = (Math.abs(start-destination)) * 264;
+    return distanceTravelled;
+}
+
+function calculatesFarePrice(start, destination){
+    let fare;
+    if (distanceTravelledInFeet(start, destination) > 2500){
+        fare = "cannot travel that far";
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000){
+        fare = 25;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400){
+        let calculatedDistance = distanceTravelledInFeet(start, destination) - 400;
+        fare = calculatedDistance * 0.02;
+    }
+    else {
+        fare = 0;
+    }
+    return fare;
+    }
